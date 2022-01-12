292d7c470dd8734a91636f202da0ef39171c4431
Aligator-cyber <o.tim.2014@mail.ru>
2022-01-12 20:47:40 +0300

    www

--- js/main.js
+++ js/main.js
@@ -26,47 +26,49 @@ select.addEventListener('change',
          });
    });
 
-function randomInteger(min, max) {
-   let i = min + Math.random() * (max - min);
-   return Math.round(i);
-}
+// function randomInteger(min, max) {
+//    let i = min + Math.random() * (max - min);
+//    return Math.round(i);
+// }
 
 
-fetch("https://newsapi.org/v2/top-headlines?country=ru&apiKey=141e8550b034469e9801e652d3166b3b")
-   .then(function (resp) { return resp.json() })
-   .then(function (data) {
-      const box = document.querySelector('#news')
-      let randomNumber = randomInteger(0, 18)
-      document.querySelector('#news__title').innerHTML = data.articles[`${randomNumber}`].title
-      document.querySelector('#news__descr').innerHTML = data.articles[`${randomNumber}`].description
-      let a = document.createElement('a');
-      a.innerHTML = data.articles[`${randomNumber}`].url
-      a.href = data.articles[`${randomNumber}`].url
-      a.style.color = 'black'
-      box.appendChild(a);
+// fetch("https://newsapi.org/v2/top-headlines?country=ru&apiKey=141e8550b034469e9801e652d3166b3b")
+//    .then(function (resp) { return resp.json() })
+//    .then(function (data) {
+//       const box = document.querySelector('#news')
+//       let randomNumber = randomInteger(0, 18)
+//       console.log(randomNumber);
+//       console.log(data);
+//       document.querySelector('#news__title').innerHTML = data.articles[`${randomNumber}`].title
+//       document.querySelector('#news__descr').innerHTML = data.articles[`${randomNumber}`].description
+//       let a = document.createElement('a');
+//       a.innerHTML = data.articles[`${randomNumber}`].url
+//       a.href = data.articles[`${randomNumber}`].url
+//       a.style.color = 'black'
+//       box.appendChild(a);
 
-      const box2 = document.querySelector('#news2')
-      let randomNumber2 = randomInteger(0, 18)
-      console.log(randomNumber2);
-      document.querySelector('#news__title2').innerHTML = data.articles[`${randomNumber2}`].title
-      document.querySelector('#news__descr2').innerHTML = data.articles[`${randomNumber2}`].description
-      let a2 = document.createElement('a');
-      a2.innerHTML = data.articles[`${randomNumber2}`].url
-      a2.href = data.articles[`${randomNumber2}`].url
-      a2.style.color = 'black'
-      box2.appendChild(a2);
+//       const box2 = document.querySelector('#news2')
+//       let randomNumber2 = randomInteger(0, 18)
+//       console.log(randomNumber2);
+//       document.querySelector('#news__title2').innerHTML = data.articles[`${randomNumber2}`].title
+//       document.querySelector('#news__descr2').innerHTML = data.articles[`${randomNumber2}`].description
+//       let a2 = document.createElement('a');
+//       a2.innerHTML = data.articles[`${randomNumber2}`].url
+//       a2.href = data.articles[`${randomNumber2}`].url
+//       a2.style.color = 'black'
+//       box2.appendChild(a2);
 
-      const box3 = document.querySelector('#news3')
-      let randomNumber3 = randomInteger(0, 18)
-      console.log(randomNumber3);
-      document.querySelector('#news__title3').innerHTML = data.articles[`${randomNumber3}`].title
-      document.querySelector('#news__descr3').innerHTML = data.articles[`${randomNumber3}`].description
-      let a3 = document.createElement('a');
-      a3.innerHTML = data.articles[`${randomNumber3}`].url
-      a3.href = data.articles[`${randomNumber3}`].url
-      a3.style.color = 'black'
-      box3.appendChild(a3);
-   })
+//       const box3 = document.querySelector('#news3')
+//       let randomNumber3 = randomInteger(0, 18)
+//       console.log(randomNumber3);
+//       document.querySelector('#news__title3').innerHTML = data.articles[`${randomNumber3}`].title
+//       document.querySelector('#news__descr3').innerHTML = data.articles[`${randomNumber3}`].description
+//       let a3 = document.createElement('a');
+//       a3.innerHTML = data.articles[`${randomNumber3}`].url
+//       a3.href = data.articles[`${randomNumber3}`].url
+//       a3.style.color = 'black'
+//       box3.appendChild(a3);
+//    })
 
 
 
